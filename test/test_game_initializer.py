ae5ad20f7ba0c4feceedc641ddb6fb5c7392424e
John M. Alexiades <John.Alexiades@gmail.com>
2022-03-20 19:53:52 -0400

    Removed old unit test case

--- test/test_game_initializer.py
+++ test/test_game_initializer.py
@@ -47,14 +47,14 @@ class TestGameInitializer(unittest.TestCase):
         self.assertTrue(len(gi.case_file_cards) == 3)
         self.assertTrue(gi.case_file_cards[0].type == 'suspect' and gi.case_file_cards[1].type == 'weapon' and gi.case_file_cards[2].type == 'room')
     
-    def test_assign_character_names(self):
-        '''Ensure that each player has an assigned character'''
-        users = {'USER_1': 'ID_1', 'USER_2': 'ID_2', 'USER_3': 'ID_3', 'USER_4' : 'ID_4', 'USER_5': 'ID_5'}
-        gi = GameInitializer(users)
-        gi.initialize_cards()
-        gi.assign_character_names()
-        for p in gi.players:
-            self.assertTrue(p.character != None)
+    # def test_assign_character_names(self):
+    #     '''Ensure that each player has an assigned character'''
+    #     users = {'USER_1': 'ID_1', 'USER_2': 'ID_2', 'USER_3': 'ID_3', 'USER_4' : 'ID_4', 'USER_5': 'ID_5'}
+    #     gi = GameInitializer(users)
+    #     gi.initialize_cards()
+    #     gi.assign_character_names()
+    #     for p in gi.players:
+    #         self.assertTrue(p.character != None)
 
     def test_initialize_player_locations(self):
         '''Ensure that each player starts on their character's home square.'''
