34a1e9d19388d7c25ce0100f7dca6b1015efec91
John M. Alexiades <John.Alexiades@gmail.com>
2022-03-20 15:30:55 -0400

    Made the client script into a class

--- client.py
+++ client.py
@@ -11,39 +11,42 @@ from colorama import Fore, Back, Style
 init() # Invoke console coloring
 
 
-host = input("Please enter the host's IP: ")
+class Client:
+    def __init__(self):
+        self.host = '127.0.0.1'#input("Please enter the host's IP: ")
 
-try:
-    print('Attempting Connection..')
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((host, 8080))
-    print('Connection Successful!')
-except Exception as connection_failed:
-    print(f'Error: Unable to connect to host {host}. Is the server currently running?')
-
-def tx_server():
-    '''Transmit communication to server'''
-    while 1:
         try:
-            client.send(input().encode('utf-8'))
-        except Exception as tx_error:
-            print(f'Error: Unable to message server: {tx_error}')
-            break
-
-def rx_server():
-    '''Receive communication from server.'''
-    while 1:
-        try:
-            msg = client.recv(3000).decode('utf-8')
-            if msg:
-                print(msg)
-            else:
+            print('Attempting Connection..')
+            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client.connect((self.host, 8080))
+            print('Connection Successful!')
+        except Exception as connection_failed:
+            print(f'Error: Unable to connect to host {self.host}. Is the server currently running?')
+        
+    def tx_server(self):
+        '''Transmit communication to server'''
+        while 1:
+            try:
+                self.client.send(input().encode('utf-8'))
+            except Exception as tx_error:
+                print(f'Error: Unable to message server: {tx_error}')
                 break
-        except Exception as rx_error:
-            print(f'Error: Unable to receive from server: {rx_error}')
-            break
+
+    def rx_server(self):
+        '''Receive communication from server.'''
+        while 1:
+            try:
+                msg = self.client.recv(3000).decode('utf-8')
+                if msg:
+                    print(msg)
+                else:
+                    break
+            except Exception as rx_error:
+                print(f'Error: Unable to receive from server: {rx_error}')
+                break
 
 
-rx_thread = threading.Thread(target=rx_server, daemon = True) # Receive communication on a separate thread
+c = Client() # Receive communication on a separate thread
+rx_thread = threading.Thread(target=c.rx_server, daemon = True) # Receive communication on a separate thread
 rx_thread.start()
-tx_server() # Transmit communication from the main thread
+c.tx_server() # Transmit communication from the main thread
