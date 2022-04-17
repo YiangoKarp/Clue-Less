b0b4a43d9336df18c6969ccdb109829ce42a0ab9
lambomurphy44 <102193818+lambomurphy44@users.noreply.github.com>
2022-04-17 10:30:02 -0400

    Checklist class created and small edit to player class to use the checklist class on init

--- player.py
+++ player.py
@@ -1,13 +1,16 @@
 # player.py
 
+from card import Card
+from checklist import Checklist
+
 class Player:
-    def __init__(self, username, client_id, character = None, location = None, cards = None, checklist = None):
+    def __init__(self, username, client_id, character = None, location = None, cards = None):
         self.username = username
         self.client_id = client_id
         self.character = character
         self.location = location
         self.cards = cards
-        self.checklist = checklist
+        self.checklist = Checklist(cards)
         # Track if the player was moved by suggestion between turns -> will affect options on their turn
         # -> always reset at the end of player's turn
         self.was_suggested = False
